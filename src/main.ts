8b60350134cb05b3ccc1966e5d508951cf6ce8aa
kaushik2901 <kaushikjadav602@gmail.com>
2019-11-12 20:43:19 +0530

    Testing git - Kaushik

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+//this is my way of contributing to open source project ;)
+
 if (environment.production) {
   enableProdMode();
 }
